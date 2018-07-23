8af616de1be8f8075d71b41af482700870eb6a28
mankovv <mankoa@rpi.edu>
2018-07-23 18:41:50 -0400

    columns no longer cut out text

--- wall.R
+++ wall.R
@@ -22,14 +22,16 @@ WallColumn <- function(data.subset, hashtag, query.c) {
     fluidRow(
       tags$h2(hashtag)
     ),
-    tags$div(style = 'height: 780px;
-                 overflow-y: auto;
-                 overflow-x: hidden;',
-      fluidRow(
+    # tags$div(style = 'height: 780px;
+    #              overflow-y: auto;
+    #              overflow-x: hidden;',
+      fluidRow(style = 'height: 780px;
+                  overflow-y: auto;
+                  overflow-x: hidden;',
         if(nrow(data.subset) > 0) {
           lapply(1:nrow(data.subset), function(y) {
             colored.text <- ColorHashtags(data.subset$text[[y]], query.c)
-            tags$div(
+            tags$div(style='padding: 0px;',
               tags$h3(paste("@", data.subset$screen_name[[y]], sep = "")),
               tags$p(HTML(colored.text)),
               tags$header(
@@ -45,5 +47,24 @@ WallColumn <- function(data.subset, hashtag, query.c) {
         }
       )
     )
-  )
+  
+}
+
+# Color the hashtags in a string using HTML
+ColorHashtags <- function(string, query.c) {
+  string.c <- unlist(strsplit(string, "[ (\n)]"))
+  hashtag.indices <- grep("#", string.c)
+  colored.string.c <- lapply(1:length(string.c), function(x) {
+    if(x %in% hashtag.indices) {
+      if(toupper(string.c[[x]]) %in% toupper(query.c)) {
+        paste('<font color=', color.blue, '>', string.c[[x]], '</font>', sep = "")
+      } else {
+        paste('<font color=', color.red, '>', string.c[[x]], '</font>', sep = "")  
+      }
+    } else {
+      string.c[[x]]
+    }
+  })
+  colored.string <- paste(colored.string.c, collapse = " ")
+  return(colored.string)
 }
