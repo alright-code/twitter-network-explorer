a52bd8de7df86bccfaf9d058b2877ae6100d207a
mankovv <mankoa@rpi.edu>
2018-10-10 14:15:45 -0400

    nvm on the externl resolution

--- app.R
+++ app.R
@@ -61,10 +61,10 @@ campfireApp(
     )),
     fluidRow(
       column(6,
-             plotOutput("top.users.bar.extern", height = "1050px")
+             plotOutput("top.users.bar.extern", height = "920px")
              ),
       column(6,
-             plotOutput("top.hashtags.bar.extern", height = "1080px")
+             plotOutput("top.hashtags.bar.extern", height = "920px")
              )
     ),
     style = paste0("background: ", color.back, ";
